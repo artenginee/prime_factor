7f0f7ee5b19ba8d4fe10fd9b12ec4d28eed9c8a2
tae-eui.kim <taeeui.k@gmail.com>
2024-07-10 09:31:26 +0900

    [refactoring] 4/7

--- prime_factors.py
+++ prime_factors.py
@@ -3,9 +3,12 @@ class PrimeFactors:
         factors = []
         if number > 1:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
+
             else:
                 factors.append(number)
+
         return factors
 
